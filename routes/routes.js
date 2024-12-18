44a452d529b0fed324698114b9f481578b0a0445
Murali v <v.murali1818@gmail.com>
2024-12-18 20:32:52 +0530

    done

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const { addOrUpdateTarget, getUserTargets, deleteTarget } = require('../controll
 // POST route to add or update a target and associate it with the user
 router.post('/targets', addOrUpdateTarget);
 // POST route to get all targets for a specific user
-router.post('/user/targets', getUserTargets);
+router.get('/user/targets', getUserTargets);
 // DELETE route to delete a target and remove it from the user's target list
 router.delete('/targets', deleteTarget);
 module.exports = router;
