bcb1d939158ee007eeef5085e12b317df9860e80
harirajhari <hariraj121203@gmailcom>
2025-02-12 12:15:37 +0530

    dahsboard count code added

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const { addOrUpdateTarget, getUserTargets, deleteTarget, updateScanStatus } = require('../controllers/targetcontroller');
+const { targetcount } = require('../controllers/dashboardcontroller');
 // POST route to add or update a target and associate it with the user
 router.post('/targets', addOrUpdateTarget);
 // POST route to get all targets for a specific user
@@ -9,6 +10,9 @@ router.get('/user/targets', getUserTargets);
 router.delete('/targets', deleteTarget);
 // PUT route to update scan_status of a target
 router.put('/targets/scan-status', updateScanStatus);
+
+
+router.get('/count/targets',targetcount);
 
 
 module.exports = router;
